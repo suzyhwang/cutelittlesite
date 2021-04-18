0d23dea05fba4eecfd561c7c7078cf35e076f31a
Suzy Hwang <82765407+suzyhwang@users.noreply.github.com>
2021-04-19 00:30:22 +0900

    Add files via upload

---
+++ foot.php
@@ -0,0 +1,9 @@
+<p align="center">&nbsp;</p>
+<div id="layer2" style="width:200px; height:76px; position:absolute; left:405px; top:646px; z-index:1;">
+    <p align="right"><img src="bottom.jpg" width="240" height="76" border="0" usemap="#ImageMap1" alt="top" align="middle"></p>
+</div>
+<map name="ImageMap1">
+<area shape="circle" coords="118, 29, 21" href="##">
+</map></body>
+
+</html>
---
+++ head.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=euc-kr">
+<title>제목 없음</title>
+<meta name="generator" content="Namo WebEditor(Trial)">
+</head>
+
+<body bgcolor="white" text="black" link="blue" vlink="purple" alink="red">
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">&nbsp;</p>
+<p align="center">
